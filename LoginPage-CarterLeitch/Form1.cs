79401cdc036f755311073c1e510c5472f6c60865
carte <carte@LEITCHCARTER-PC>
2019-02-13 09:18:05 -0500

    Add project files.

---
+++ LoginPage-CarterLeitch/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginPage_CarterLeitch
+{
+    public partial class Form1 : Form
+    {
+        public static string  name = "Carter";
+        public static string password = "Yeet";
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoginScreen fs = new LoginScreen();
+            this.Controls.Add(fs);
+        }
+    }
+}
---
+++ LoginPage-CarterLeitch/LoginScreen.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginPage_CarterLeitch
+{
+    public partial class LoginScreen : UserControl
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+            label3.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == Form1.name && textBox2.Text == Form1.password) 
+            {
+                Form f = this.FindForm();
+                f.Controls.Remove(value: this);
+
+                AccessGrantedScreen sc = new AccessGrantedScreen();
+                f.Controls.Add(sc);
+            }
+            else
+            {
+                label3.Visible = true;
+            }
+        }
+    }
+}
